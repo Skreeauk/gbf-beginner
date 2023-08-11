bd576a7abdf4b2745e6718780d85bf86480f8445
Skreeauk <waldojongsoo@yahoo.com>
2023-08-12 04:19:11 +0800

    Core Concepts

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -13,7 +13,7 @@ export function MainNav({ items, children }) {
         useState(false)
 
     return (
-        <div className="flex gap-6 md:gap-10">
+        <div className="flex gap-6 md:gap-10 items-center">
             <Link
                 href="/"
                 className="hidden md:block"
@@ -25,47 +25,51 @@ export function MainNav({ items, children }) {
                     alt="GBF - Beginner"
                 />
             </Link>
-            {items?.length ? (
-                <nav className="hidden gap-6 md:flex">
-                    {items?.map((item, index) => (
-                        <Link
-                            key={index}
-                            href={
-                                item.disabled
-                                    ? "#"
-                                    : item.href
-                            }
-                            className={cn(
-                                "flex items-center text-lg text-foreground font-medium transition-colors hover:text-foreground/80 sm:text-sm  relative after:absolute after:bg-foreground after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300",
-                                item.disabled &&
-                                    "cursor-not-allowed opacity-80"
-                            )}
-                        >
-                            {item.title}
-                        </Link>
-                    ))}
-                </nav>
-            ) : null}
-            <button
-                className="flex items-center space-x-2 md:hidden text-foreground"
-                onClick={() =>
-                    setShowMobileMenu(!showMobileMenu)
-                }
-            >
-                {showMobileMenu ? (
-                    <Icons.close />
-                ) : (
-                    <Icons.logo />
-                )}
-            </button>
-            {showMobileMenu && items && (
-                <MobileNav
-                    items={items}
-                    setShowMobileMenu={setShowMobileMenu}
+            <div className="flex gap-6 md:gap-10 h-fit">
+                {items?.length ? (
+                    <nav className="hidden gap-6 md:flex">
+                        {items?.map((item, index) => (
+                            <Link
+                                key={index}
+                                href={
+                                    item.disabled
+                                        ? "#"
+                                        : item.href
+                                }
+                                className={cn(
+                                    "flex items-center text-lg text-foreground font-medium transition-colors hover:text-foreground/80 sm:text-sm  relative after:absolute after:bg-foreground after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300",
+                                    item.disabled &&
+                                        "cursor-not-allowed opacity-80"
+                                )}
+                            >
+                                {item.title}
+                            </Link>
+                        ))}
+                    </nav>
+                ) : null}
+                <button
+                    className="flex items-center space-x-2 md:hidden text-foreground"
+                    onClick={() =>
+                        setShowMobileMenu(!showMobileMenu)
+                    }
                 >
-                    {children}
-                </MobileNav>
-            )}
+                    {showMobileMenu ? (
+                        <Icons.close />
+                    ) : (
+                        <Icons.logo />
+                    )}
+                </button>
+                {showMobileMenu && items && (
+                    <MobileNav
+                        items={items}
+                        setShowMobileMenu={
+                            setShowMobileMenu
+                        }
+                    >
+                        {children}
+                    </MobileNav>
+                )}
+            </div>
         </div>
     )
 }
